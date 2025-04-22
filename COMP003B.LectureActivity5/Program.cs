baf604750818cecc3da53d64a92e60527c2a310f
flores-97 <floresvictor15@gmail.com>
2025-04-21 18:20:08 -0700

    step 2 complete. moving to step 3 of lecture activity

--- COMP003B.LectureActivity5/Program.cs
+++ COMP003B.LectureActivity5/Program.cs
@@ -1,3 +1,4 @@
+
 namespace COMP003B.LectureActivity5
 {
     public class Program
@@ -6,40 +7,28 @@ namespace COMP003B.LectureActivity5
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            // Add services to the container.
+
             builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
+            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-            /*// Add services to the container.
-            builder.Services.AddControllersWithViews();
 
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            */
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
 
             app.UseAuthorization();
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.MapControllers();
 
             app.Run();
         }
